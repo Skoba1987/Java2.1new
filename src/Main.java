6d2681e5ab193442381b63d90e8d3b5b01c3f952
Skoba Kristina <kfisher@mail.ru>
2022-07-16 16:05:50 +0300

    fix gitignore

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 2000; // стоимость билета
+        int miles = 20; // количество рублей для одной бонусной милли//
+
+        System.out.println("За покупку билета начислено миль");
+        System.out.println(price / miles);
+
+    }
+}
+
